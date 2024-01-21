94f064f5a585bb9f3cf07034d96f56ac4232f084
Deglier <deglier.carneiro@gmail.com>
2024-01-21 17:57:27 -0300

    estudando header 1

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <main className="w-screen h-screen">
+            <header className="bg-[#365486] w-full h-20 flex items-center px-5 justify-between">
+                <div className="flex">
+                    <h2 className="text-white text-3xl">Simple</h2><h2 className="text-red-600 text-3xl">X</h2>
+                </div>
+            </header>
+        </main>
+    )
+}
